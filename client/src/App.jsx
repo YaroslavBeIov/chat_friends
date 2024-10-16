9ddc543c07e4aeda47877903b2862d2d42e8561c
YaroslavBeIov <yaroslav.shurko@mail.ru>
2024-10-16 08:05:58 +0300

    Routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
+import socketIO from 'socket.io-client'
+import Home from './components/home/home.jsx'
+import ChatPage from './components/chat/index.jsx'
+import {Route, Routes} from 'react-router-dom'
+const socket = socketIO.connect('http://localhost:5000')
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Routes>
+      <Route path='/' element= {<Home />} />
+      <Route path='/chat' element= {<ChatPage />} />
+    </Routes>
   )
 }
 
-export default App
+export default App;
